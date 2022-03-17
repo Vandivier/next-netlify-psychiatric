64ebda671f7d85b39924df9ba1f3e987d6ddad24
Vandivier <john@afterecon.com>
2022-03-17 18:30:37 -0400

    feat: stub google map

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -1,1 +1,28 @@
-export const MapComponent = () => <p>I AM A MAP COMPONENT TODO</p>;
+import GoogleMapReact from "google-map-react";
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+export const MapComponent = () => {
+  // ref: https://stackoverflow.com/questions/52248646/googlemaps-api-key-for-testing
+  const key = "";
+  const stubData = {
+    center: {
+      lat: 59.95,
+      lng: 30.33,
+    },
+    zoom: 11,
+  };
+
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key }}
+        defaultCenter={stubData.center}
+        defaultZoom={stubData.zoom}
+      >
+        <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+      </GoogleMapReact>
+    </div>
+  );
+};
